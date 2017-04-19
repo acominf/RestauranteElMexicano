469b480527291f204d9f09424c9eba01ccdcd4f9
yairTerrones <yair82010@hotmail.com>
2017-04-18 20:49:43 -0500

    Se agrego una caja animada.

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,19 @@
 
 $(document).ready(function () {
 
+    /*Logo principal situado en la parte de arriba. */
+    $('#LogoPrincipal').mouseenter(function(){
+        $(this).animate({
+            height: '+=20px',
+            width: '+=20px'
+        });
+    })
+
+    $('#LogoPrincipal').mouseleave(function(){
+        $(this).animate({
+            height: '-=20px',
+            width: '-=20px'
+        });
+    })
+    /*Fin logo principal. */
 });
