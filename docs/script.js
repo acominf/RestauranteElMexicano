f33df79f70698017e1a2dcf25aed5680121e7845
yairTerrones <yair82010@hotmail.com>
2017-04-27 21:51:17 -0500

    Se creo un menu con animacion.

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function () {
 
-    /*Logo principal situado en la parte de arriba. */
+    /*guarda la altura entre la parte dsuperior del sitio weub y el menu */
+    var altura = $('.menu').offset().top;  
+    /* */
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > altura){
+            $('.menu').addClass('menu-fixed');
+        }else{
+            $('.menu').removeClass('menu-fixed');
+        }
+    });
+
+    /*Logo principal situado en la parte de arriba. 
     $('#LogoPrincipal').mouseenter(function(){
         $(this).animate({
             height: '+=20px',
@@ -14,6 +25,6 @@ $(document).ready(function () {
             height: '-=20px',
             width: '-=20px'
         });
-    })
-    /*Fin logo principal. */
+    }) */
+    
 });
