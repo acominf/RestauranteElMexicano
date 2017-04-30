3196e78d182492b8bb57b179981520bc1b30dd95
yairTerrones <yair82010@hotmail.com>
2017-04-30 15:53:40 -0500

    Se agrego un slide para la pag principal.

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,7 @@
 
 $(document).ready(function () {
-
-    /*guarda la altura entre la parte dsuperior del sitio weub y el menu */
-    var altura = $('.menu').offset().top;  
+    /*Modificacion del menu */
+    var altura = $('.menu').offset().top;/*guarda la altura entre la parte dsuperior del sitio web y el menu */
     /* */
     $(window).on('scroll',function(){
         if($(window).scrollTop() > altura){
@@ -12,7 +11,57 @@ $(document).ready(function () {
         }
     });
 
-    /*Logo principal situado en la parte de arriba. 
+    /** slide */
+    $('#btnAnt').click(function(){
+        /**Encontramo el total de elementos que tiene .slider */
+        var tam = $('.slider').find('.elementSlider').length;
+        /**Recorremos cada una de ellas */
+        $('.slider').find('.elementSlider').each(
+            function(index,value) {
+                if($(value).hasClass("svisible")) {
+                    /**Quitamos la imagen que se encuentra en ese momoento */
+                    $(value).slideUp();
+                    $(value).removeClass('svisible');
+                    /**Si es el primero */
+                    if(index == 0){
+                        $($('.slider').find('.elementSlider').get(tam-1)).slideDown();
+                        $($('.slider').find('.elementSlider').get(tam-1)).addClass('svisible');
+                        return false;
+                    }else{
+                        $($('.slider').find('.elementSlider').get(index - 1)).slideDown();
+                        $($('.slider').find('.elementSlider').get(index - 1)).addClass('svisible');
+                        return false;
+                    }
+                }
+            });
+        });
+
+        $('#btnSig').click(function(){
+        /**Encontramo el total de elementos que tiene .slider */
+        var tam = $('.slider').find('.elementSlider').length;
+        /**Recorremos cada una de ellas */
+        $('.slider').find('.elementSlider').each(
+            function(index,value) {
+                if($(value).hasClass("svisible")) {
+                    /**Quitamos la imagen que se encuentra en ese momoento */
+                    $(value).slideUp();
+                    $(value).removeClass('svisible');
+                    /**Si es el primero */
+                    if(index+1 < tam){
+                        $($('.slider').find('.elementSlider').get(index+1)).slideDown();
+                        $($('.slider').find('.elementSlider').get(index+1)).addClass('svisible');
+                        return false;
+                    }else{
+                        $($('.slider').find('.elementSlider').get(0)).slideDown();
+                        $($('.slider').find('.elementSlider').get(0)).addClass('svisible');
+                        return false;
+                    }
+                }
+            });
+        });
+});
+
+  /*Logo principal situado en la parte de arriba. 
     $('#LogoPrincipal').mouseenter(function(){
         $(this).animate({
             height: '+=20px',
@@ -26,5 +75,3 @@ $(document).ready(function () {
             width: '-=20px'
         });
     }) */
-    
-});
