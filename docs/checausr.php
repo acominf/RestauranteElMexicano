13d043847fdd0e0745fb4a0c3d85f86735c75f8e
yairTerrones <yair82010@hotmail.com>
2017-05-28 16:06:21 -0500

    Achivos login.

---
+++ docs/checausr.php
@@ -0,0 +1,6 @@
+<?php
+    if(isset($_GET['usuario']))
+    {
+        echo('Hola').$_GET['usuario'];
+    }
+?>
--- docs/index.php
+++ docs/index.php
@@ -1,25 +1,25 @@
 <?php session_start();
 
-    if($_SERVER['REQUEST_METHOD']=='POST')
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $conexion = new mysqli('localhost', 'root','','ejemplo2');
+    $usuario = $conexion->($_POST['usuario']);
+    $contraseña = $conexion->($_POST['contraseña']);
+    //$contraseña = hash('md5','$o#'. $contraseña.'@8!');
+    
+    $consulta = "SELECT * FROM USUARIOS WHERE username = '$usuario' AND
+    password = '$contraseña'";
+    $resultado = $conexion->query($consulta);
+    if($resultado->num_rows == 1)
     {
-        $conexion = new mysqli('localhost', 'root','','ejemplo2');
-        $usuario = $conexion->real_escape_string($_POST['usuario']);
-        $contraseña = $conexion->real_escape_string($_POST['contraseña']);
-        $contraseña = hash('md5','$o#'.$contraseña.'@8!');
-        //$consulta = "SELECT * FROM USUARIOS WHERE username = '$usuario' AND password = '$contraseña'";
-        $consulta = "SELECT * FROM USUARIOS WHERE nombre = '$usuario' AND password = '$contraseña'";
-        $resultado = $conexion->query($consulta);
-
-        if($resultado->num_rows == 1)
-        {
-            $_SESSION['usuario'] = $usuario;
-            header('Location: contenido.php');
-        }
-        else
-        {
-            echo("<h1>Error: Usuario o contraseña no validos. </h1>");
-        }
+        $_SESSION['usuario']= $usuario;
+        header('Location: contenido.php');
     }
+    else
+    {
+        echo "usuario no valido";
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -28,23 +28,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pagina sesiones</title>
+    <title>Sesiones</title>
 </head>
 <body>
-    <h1> Iniciar sesion </h1>
+    <h1> Ejemplo de sesiones </h1>
     <form method="POST">
-        Usuario: <input type="text" name="usuario">
-        <br>
-        Contraseña: <input type="password" name="contraseña">
-        <br>
-        <input type= "submit" value="Ingresar">
-        <!--<br>
-        <br>
-        <a href="contenido.php"> Contenido sesion</a>
-        <br>-->
-        <br>
-        <p> No te has registrado? </p>
-        <a href="registro.php"> Registrate aqui </a>
+    Usuario: <input type="text" name="usuario">
+    Contraseña: <input type="password" name="contraseña">
+    <input type= "submit">
     </form>
+    <a href="contenido.php"> Iniciar sesion</a>
+    <a  href="registro.php">Registrate aqui</a>
 </body>
 </html>
