5603e61e1b3a21134cfa0fd24decb2ac8254a49e
yairTerrones <yair82010@hotmail.com>
2017-05-30 22:51:06 -0500

    Se agrego la forma de registro.

--- docs/registro.php
+++ docs/registro.php
@@ -49,18 +49,55 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Iniciar sesión.</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <script src="jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-    <h1>Ejemplo de registro</h1>
-    <form method="POST">
-    Nombre de Usuario: <input type="text" name="usuario"><br>
-    Telefono: <input type="text" name="telefono"><br>
-    Contraseña: <input type="password" name="contraseña"><br>
-    Contraseña Nuevamenten: <input type="password" name="contraseña2"><br>
-    <input type= "submit" value= "registrar">
-    </form>
+    <div class="contenedorMenu">
+        <header>
+          <div class="logo" id="logoPrincipal">
+            <a href="index.php"><img src="logoPrincipal.png" alt="Principal"></a>
+          </div>
+        <nav class="menu">
+              <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="servicios.html">Servicios</a></li>
+                <li><a href="variedad.html">Variedad</a></li>
+                <li><a href="historia.html">Historia</a></li>
+                <li class="separado"><a href="acercaDe.html">A cerca de..</a></li>
+                <?php 
+                if(!isset($_SESSION['usuario'])) { //Si no esta iniciada la sesion
+                    echo '<li><a href="index1.php"><u>Inciar sesión</u></a></li>';
+                    echo '<li><a href="registro.php"><u>Registrase</u></a></li>';
+                }else {
+                  $usuario = $_SESSION['usuario'];
+                    echo '<li><a href=""><u>'.$usuario.'</u></a></li>';
+                    echo '<li><a href="cerrar.php"><u>Cerrar sesión</u></a></li>';
+                }
+                ?>
+              </ul>
+          </nav>
+    </header>
+    </div>
+
+    <main>
+        <div class = "inicioSesion">
+        <h1>Registro.</h1>
+            <div class="divDatos registro">
+                <form method="POST">
+                    Nombre de Usuario: <input type="text" name="usuario"><br><br>
+                    Telefono: <input type="text" name="telefono"><br><br>
+                    Contraseña: <input type="password" name="contraseña"><br><br>
+                    Confirmar contraseña: <input type="password" name="contraseña2"><br><br>
+                    <center>
+                        <input type= "submit" value= "Registrar" class="btnCentrado">
+                    </center>
+                </form>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
